5c31642ac37b7e2982a6230934f7168b6831499a
makeyzn <tntmr774@gmail.com>
2023-02-14 22:43:43 +0300

    components 1.2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
-import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css'
 
 function App() {
-  const [likes, setLikes] = useState(1);
-  const [value, setValue] = useState('input');
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description'},
+    {id: 3, title: 'JavaScript 3', body: 'Description'},
+  ])
 
-
-
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},
+  ])
 
   return (
     <div className="App">
-      <ClassCounter />
+      <PostList posts={posts} title="Посты про JS"/>
+      <PostList posts={posts2} title="Посты про Python"/>
     </div>
   );
 }
