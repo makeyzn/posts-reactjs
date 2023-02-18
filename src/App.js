03cf08dad0f141b8414c07d5dd535d003d0bd87d
makeyzn <tntmr774@gmail.com>
2023-02-19 00:23:41 +0300

    add Modal window

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import MyModal from "./components/UI/MyModal/MyModal";
 import MySelect from "./components/UI/select/MySelect";
 import './styles/App.css'
 
@@ -17,6 +18,7 @@ function App() {
 
   const [filter, setFilter] = useState({sort: '', query: ''});
 
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo( () => {
     console.log('Отработала функция getSortedPosts')
@@ -32,6 +34,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false);
   }
 
   // получаем post из дочернего компонента
@@ -43,8 +46,13 @@ function App() {
 
   return (
     <div className="App">
-      {/* create={createPost} -- Функция обратного вызова. Вызываем ее внутри компонента PostForm */}
-      <PostForm create={createPost}/>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal} >
+        {/* create={createPost} -- Функция обратного вызова. Вызываем ее внутри компонента PostForm */}
+        <PostForm create={createPost}/>
+      </MyModal>
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
       <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
