0f2e8de3d1977073072c52cd2dc79ca040f1fbe9
makeyzn <tntmr774@gmail.com>
2023-02-19 17:50:54 +0300

    pag with useEff

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   useEffect(() => {
     fetchPosts();
-  }, [])
+  }, [page])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -49,7 +49,6 @@ function App() {
 
   const changePage = (page) => {
     setPage(page);
-    fetchPosts();
   }
 
   return (
